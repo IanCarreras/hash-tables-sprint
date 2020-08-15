f88050a3819980371405db36946479abed4cc16e
Ian <mus5002@aol.com>
2020-08-15 13:35:48 -0400

    completed ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,22 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    # iterate over each  list and sublist
+    # store element as key and counter as value
+    # if key in h has value = length of the given array of arrays
+    # key exists in all suybarrays
+    h = {}
+    results = []
+    for l in arrays:
+        for i in l:
+            if i not in h:
+                h[i] = 1
+            else:
+                h[i] +=1
+            if h[i] == len(arrays):
+                results.append(i)
 
-    return result
+    return results
 
 
 if __name__ == "__main__":
