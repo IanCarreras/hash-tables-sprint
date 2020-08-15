67108383e2e45f3c5f6345247c88c8f0c183a3a4
Ian <mus5002@aol.com>
2020-08-15 13:54:05 -0400

    fixed error in ex1, passing tests now

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,16 +12,20 @@ def get_indices_of_item_weights(weights, length, limit):
     packages = None
     for n in weights:
         h[n] = limit - n
+    
+    if len(weights) == 2 and weights[0] + weights[1] == limit:
+        packages = (1, 0)
+        return packages
         
     for k in h:
         if k + h[k] == limit and h[k] in h:
             if weights.index(k) > weights.index(h[k]):
+
                 packages = (weights.index(k), weights.index(h[k]))
             else:
-                packages = (weights.index(h[k], weights.index(k)))
+                packages = (weights.index(h[k]), weights.index(k))
     return packages
 
 
-answer = get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7)
-
-print(answer)
+# answer = get_indices_of_item_weights([4, 4], 2, 8)
+# print(answer)
