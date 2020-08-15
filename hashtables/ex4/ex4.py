471e9340a6e47a7e501f00b304af505ba7ea2abc
Ian <mus5002@aol.com>
2020-08-15 13:14:23 -0400

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # iterate through list of integers
+    # store abs() of int as key in hashtable with a count value
+    h = {}
+    both = []
+    for i in a:
+        i = abs(i)
+        if i not in h:
+            h[i] = 1
+        else:
+            both.append(i)
 
-    return result
+    return both
 
 
 if __name__ == "__main__":
